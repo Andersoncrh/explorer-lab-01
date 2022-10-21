d391de644c2d18b01a51a5eff14894910cd43564
Andersoncrh <andersoncrh@gmail.com>
2022-10-21 17:29:56 -0300

    final aula 1, bandeiras elo e alelo adicionadas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) path"
+)
+const ccBgColor02 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(2) path"
+)
+const ccLogo = document.querySelector(
+  ".cc-logo span:nth-child(2) img"
+)
+
+function setCardType(type){
+  const colors = {
+    visa: ["#1839E5", "#EC9444"],
+    mastercard: ["#C69347", "#980606"],
+    elo: ["#00A4E0", "#EF4123"],
+    alelo: ["#007858", "#FFFFFF"],
+    default: ["black", "grey"],
+  }
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+globalThis.setCardType = setCardType
